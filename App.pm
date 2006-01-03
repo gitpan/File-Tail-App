48ffe8df9b9064360667f5c0e5057a9c40927570
Daniel Muey <dmuey@cpan.org>
2006-01-03 22:28:34 +0000

    Import of DMUEY/File-Tail-App-v0.0.2 from CPAN.
    gitpan-cpan-distribution: File-Tail-App
    gitpan-cpan-version:      v0.0.2
    gitpan-cpan-path:         DMUEY/File-Tail-App-v0.0.2.tar.gz
    gitpan-cpan-author:       DMUEY
    gitpan-cpan-maturity:     released

--- App.pm
+++ App.pm
@@ -1,7 +1,5 @@
 package File::Tail::App;
 
-# - killed truncated restarted = infiloop
-
 use strict;
 use warnings;
 
@@ -12,7 +10,7 @@ require Exporter;
 our @ISA    = qw(Exporter);
 our @EXPORT = qw(tail_app);
 
-use version;our $VERSION = qv('0.0.1');
+use version;our $VERSION = qv('0.0.2');
 
 sub File::Tail::seek_to {
     my($tail, $seek_to) = @_;
@@ -31,13 +29,14 @@ sub File::Tail::app {
     $args_ref->{'verbose'} = 0 if !defined $args_ref->{'verbose'};
 
     my $lastrun_file = $args_ref->{'lastrun_file'};
+    my $do_md5_check = $args_ref->{'do_md5_check'} || 0;
 
-    my($previous_position, $file_ident) = defined $lastrun_file
-                                              ? _get_lastrun_data(
-                                                    $tail->{'input'}, 
-                                                    $lastrun_file
-                                                )
-                                              : ('','');
+    my($previous_position, $file_ident, $md5_chk, $md5_len) 
+        = defined $lastrun_file ? _get_lastrun_data($tail->{'input'}, 
+                                                    $lastrun_file,
+                                                    $do_md5_check, 
+                                                    $tail)
+                                : ('','','','');
 
     $args_ref->{'seek_on_zero'} = 1 if !exists $args_ref->{'seek_on_zero'};
     if(exists $args_ref->{'seek_to'} && defined $args_ref->{'seek_to'}) {
@@ -49,28 +48,58 @@ sub File::Tail::app {
         }
     }
 
-    my $start_size = -s $tail->{'input'};
+    my $start_size   = -s $tail->{'input'};
+    my $start_handle = $tail->{'handle'}; 
+
     $tail->seek_to($previous_position) if $previous_position ne $start_size
                                           && $previous_position ne '';
 
     while( defined( my $line = $tail->read() ) ) {
         my @stat = stat $tail->{'input'};
-
+        my $replaced = 0;
         if(-s $tail->{'input'} < $start_size) {
             carp "$tail->{'input'} was truncated: " . tell($tail->{'handle'})
                 if $args_ref->{'verbose'};
             $tail->seek_to(length $line); 
-            $start_size = $stat[7];
+            $replaced++;
         } 
-        elsif($stat[1] ne $file_ident && $file_ident) {
-            carp "$tail->{'input'} was replaced: " . tell($tail->{'handle'})
+        elsif($do_md5_check && $md5_chk ne _get_md5_info($tail, 
+                                                       $md5_len, 
+                                                       $do_md5_check)) {
+            carp "MD5 Check changed: " . tell($tail->{'handle'})
+                if $args_ref->{'verbose'};
+            $replaced++;
+        }
+
+        if($replaced) {
+            $tail->seek_to(length $line);
+            $start_size = $stat[7];
+            $md5_len = $stat[7] < 42  ? $stat[7] : 42;
+            $md5_chk = _get_md5_info($tail, $md5_len, $do_md5_check);
+        }
+
+        # do simple checks then tell them about it & reset some vars if needed
+        if($stat[1] ne $file_ident && $file_ident) {
+            carp "$tail->{'input'} was replaced: " . tell($tail->{'handle'}) 
                 if $args_ref->{'verbose'};
             $file_ident = $stat[1];
         }
+ 
+        if($start_handle ne $tail->{'handle'}) {
+            # checking descriptor via fileno() is same check but numerically
+            carp "descriptor/handle changed: " . tell($tail->{'handle'})
+                if $args_ref->{'verbose'};
+            $start_handle = $tail->{'handle'};
+        }
 
         $args_ref->{'line_handler'}->($line);
-        _set_lastrun_data(tell($tail->{'handle'}),$file_ident,$lastrun_file)
-            if defined $lastrun_file;
+        _set_lastrun_data( 
+                           tell($tail->{'handle'}),
+                           $file_ident,
+                           $md5_chk, 
+                           $md5_len, 
+                           $lastrun_file
+                         ) if defined $lastrun_file;
         carp "$tail->{'input'} is at : " . tell($tail->{'handle'})
             if $args_ref->{'verbose'} > 1;
     }   
@@ -85,8 +114,8 @@ sub tail_app {
     croak 'missing "new" key from tail_app arg' if !exists $args_ref->{'new'};
     croak '"new" must be an array ref' if ref $args_ref->{'new'} ne 'ARRAY';
 
-   # local $Carp::CarpLevel = 1; # since new croaks on failure (and read() ?)
-    my $tail = File::Tail->new(@{ $args_ref->{'new'} });
+    my $tail = File::Tail->new(@{ $args_ref->{'new'} }) 
+        or croak "Could not create File::Tail object: $!";
 
     $tail->app({
         'line_handler' => $args_ref->{'line_handler'},
@@ -94,36 +123,62 @@ sub tail_app {
         'seek_to'      => $args_ref->{'seek_to'},
         'seek_on_zero' => $args_ref->{'seek_on_zero'},
         'lastrun_file' => $args_ref->{'lastrun_file'}, 
+        'do_md5_check' => $args_ref->{'do_md5_check'},
     });
 }
 
 sub _get_lastrun_data {
-    my($tail_file, $cur_file) = @_;
+    my($tail_file, $cur_file, $do_md5_check, $tail) = @_;
 
     my @stat              = stat $tail_file;
     my $previous_position = 0; # start at zero if 1st time ...
     my $cur_tail_ident    = $stat[1]; #:$stat[10]# or if file's changed
+    my $_md5_len          = $stat[7] < 42  ? $stat[7] : 42; 
+    my $_md5_chk          = _get_md5_info($tail, $_md5_len, $do_md5_check);
 
+    my($curpos, $logged_ident, $md5_chk, $md5_len) = ('','',$_md5_chk,$_md5_len);
     if(-e $cur_file) {
         open my $curat_fh, '<', $cur_file 
             or croak "Could not read $cur_file: $!";
         chomp(my $first_line = <$curat_fh>);
         close $curat_fh;
 
-        my($curpos, $logged_ident) = split /-/, $first_line;
+        ($curpos, $logged_ident, $md5_chk, $md5_len) = split /-/, $first_line;
         $curpos = 0 if $logged_ident ne $cur_tail_ident;
         $previous_position = int($curpos);
+       
+        if($do_md5_check) {
+           $md5_len = $_md5_len if !defined $md5_len || !$md5_len; 
+           $md5_chk = $_md5_chk if !defined $md5_chk || !$md5_chk;
+        }
     }
 
-    return ($previous_position, $cur_tail_ident);
+    return ($previous_position, $cur_tail_ident, $md5_chk, $md5_len);
 }
 
 sub _set_lastrun_data {
-    my($new_posi, $file_ident, $cur_file) = @_;
+    my($new_posi, $file_ident, $md5_chk, $md5_len, $cur_file) = @_;
+
     open my $curpos_fh, '>', $cur_file 
         or croak "Could not write $cur_file: $!";
-    print {$curpos_fh} qq($new_posi-$file_ident);
+    print {$curpos_fh} qq($new_posi-$file_ident-$md5_chk-$md5_len);
     close $curpos_fh;
+}
+
+sub _get_md5_info {
+    my($tail, $md5_len, $do_md5_check) = @_;
+
+    return if !$do_md5_check;
+    require Digest::MD5; # only do the module if needed
+
+    my $data_to_md5 = ''; # to avoid uninitialized value warnings
+    my $origpos = tell($tail->{'handle'});
+
+    $tail->seek_to(0);
+    sysread $tail->{'handle'}, $data_to_md5, $md5_len;
+    $tail->seek_to($origpos);
+
+    return Digest::MD5::md5_hex($data_to_md5);
 }
 
 1;
@@ -188,6 +243,10 @@ If you'd rather do this your self the you'll need to get the current position be
 
 The lastrun_file parameter handles all of that and more for you. Feel free to take a look at how it works if you need to write your own for some reason, like you want to use SQL DB instead of a file, etc etc...
 
+=item do_md5_check
+
+If using lastrun_file also do a check on the MD5 sum of a small part of data in the beginning of the file to see if its been truncated and handle appropriately. (Thanks to Ben Thomas for the MD5 idea!!)
+
 =back
 
 =head2 $tail->app()
@@ -220,7 +279,7 @@ This example will process x.log as its updated. To avoid double logging it only 
     use File::Tail::App qw(tail_app);
 
     tail_app({
-        'new'          => ["x.log"],
+        'new'          => ['x.log'],
         'line_handler' => \&_wag_tail,
         'lastrun_file' => 'x.lastrun',
     });
